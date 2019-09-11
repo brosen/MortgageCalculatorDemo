440c7dac9e3660495a8e2b28a9b907a5ba281ea9
brad rosen <brosen@mdsol.com>
2019-09-11 19:58:31 -0400

    incomplete

--- MortgageCalculatorDemo/ClientApp/src/components/MortgageSummary.js
+++ MortgageCalculatorDemo/ClientApp/src/components/MortgageSummary.js
@@ -14,6 +14,10 @@ export class MortgageSummary extends Component {
     this.handleChange = this.handleChange.bind(this);
 
     this.onChangeLoanAmount = this.onChangeLoanAmount.bind(this);
+    this.onChangeIntrestRate = this.onChangeIntrestRate.bind(this);
+    this.onChangeLifeOfLoan = this.onChangeLifeOfLoan.bind(this);
+    this.onChangePaymentsPerYear = this.onChangePaymentsPerYear.bind(this);
+    this.CalculatePaymentsPerPeriod = this.CalculatePaymentsPerPeriod.bind(this);
   }
 
   handleChange(e) {
@@ -29,30 +33,45 @@ export class MortgageSummary extends Component {
     });
   }
   handleClick() {
-    let newItems = [];
-    for (var i = 0; i < this.state.currentCount; i++) {
-      newItems.push(i + 1);
-    }
-    this.setState({
-      items: newItems
-    });
+    //let newItems = [];
+    //for (var i = 0; i < this.state.currentCount; i++) {
+    //  newItems.push(i + 1);
+    //}
+    //this.setState({
+    //  items: newItems
+    //});
+    console.log(this.props.mortgageSummary);
   }
 
   onChangeLoanAmount(e) {
     this.props.changeLoanAmount(e.target.value);
   }
+  onChangeIntrestRate(e) {
+    this.props.changeIntrestRate(e.target.value);
+  }
+  onChangeLifeOfLoan(e) {
+    this.props.changeLifeOfLoan(e.target.value);
+  }
+  onChangePaymentsPerYear(e) {
+    this.props.changePaymentsPerYear(e.target.value);
+  }
 
   render() {
     let loanAmount = this.props.mortgageSummary.loanAmount;
+    let intrestRate = this.props.mortgageSummary.intrestRate;
+    let lifeOfLoan = this.props.mortgageSummary.lifeOfLoan;
+    let paymentsPerYear = this.props.mortgageSummary.paymentsPerYear;
+    let totalPayments = lifeOfLoan * paymentsPerYear;
+    let paymentsPerPeriod = this.CalculatePaymentsPerPeriod();
     return (
       <div>
         <h1>Mortgage Calculator Summary</h1>
         <div>Loan Amount <input type="text" value={loanAmount} onChange={this.onChangeLoanAmount} /></div>
-        <div>Annual Intrest Rate <input type="text" value={loanAmount} onChange={this.onChangeLoanAmount} /></div>
-        <div>Life loan (in years) <input type="text" value={this.state.value} onChange={this.handleChange} /></div>
-        <div>Number of payments per year <input type="text" value={this.state.value} onChange={this.handleChange} /></div>
-        <div>Total number of payments <input type="text" value={this.state.value} onChange={this.handleChange} /></div>
-        <div>Payments per period <input type="text" value={this.state.value} onChange={this.handleChange} /></div>
+        <div>Annual Intrest Rate <input type="text" value={intrestRate} onChange={this.onChangeIntrestRate} /></div>
+        <div>Life loan (in years) <input type="text" value={lifeOfLoan} onChange={this.onChangeLifeOfLoan} /></div>
+        <div>Number of payments per year <input type="text" value={paymentsPerYear} onChange={this.onChangePaymentsPerYear} /></div>
+        <div>Total number of payments <input type="text" value={totalPayments} disabled/></div>
+        <div>Payments per period <input type="text" value={paymentsPerPeriod} disabled/></div>
         <div>Sum of payments <input type="text" value={this.state.value} onChange={this.handleChange} /></div>
         <div>Intrest cost <input type="text" value={this.state.value} onChange={this.handleChange} /></div>
         <div>  <button onClick={this.handleClick}>Run</button></div>
--- MortgageCalculatorDemo/ClientApp/src/store/MortgageSummary.js
+++ MortgageCalculatorDemo/ClientApp/src/store/MortgageSummary.js
@@ -1,8 +1,18 @@
 ﻿// import { axiosGet } from './HttpUtility';
 
 const CHANGE_LOAN_AMOUNT = 'CHANGE_LOAN_AMOUNT';
+const CHANGE_INTREST_RATE = 'CHANGE_INTREST_RATE';
+const CHANGE_LIFE_OF_LOAN = 'CHANGE_LIFE_OF_LOAN';
+const CHANGE_PAYMENTS_PER_YEAR = 'CHANGE_PAYMENTS_PER_YEAR';
+
 export const API_ENDPOINT = 'api/SampleData/';
-const initialState = { loanAmount: 0 };
+
+const initialState = {
+  loanAmount: 0,
+  intrestRate: 0,
+  lifeOfLoan: 0,
+  paymentsPerYear: 0
+};
 
 /*
     const url = API_ENDPOINT + 'summary';
@@ -17,8 +27,29 @@ export const actionCreators = {
       type: CHANGE_LOAN_AMOUNT,
       amount: amount
     });
+  },
+  changeIntrestRate: rate => async (dispatch, getState) => {
+    dispatch({
+      type: CHANGE_INTREST_RATE,
+      rate: rate
+    });
+  },
+  changeLifeOfLoan: lifeOfLoan => async (dispatch, getState) => {
+    dispatch({
+      type: CHANGE_LIFE_OF_LOAN,
+      lifeOfLoan: lifeOfLoan
+    });
   }
+  ,
+  changePaymentsPerYear: paymentsPerYear => async (dispatch, getState) => {
+    dispatch({
+      type: CHANGE_PAYMENTS_PER_YEAR,
+      paymentsPerYear: paymentsPerYear
+    });
+  }
+
 };
+
 
 
 export const reducer = (state, action) => {
@@ -26,6 +57,18 @@ export const reducer = (state, action) => {
 
   if (action.type === CHANGE_LOAN_AMOUNT) {
     return { ...state, loanAmount: action.amount };
+  }
+
+  if (action.type === CHANGE_INTREST_RATE) {
+    return { ...state, intrestRate: action.rate };
+  }
+
+  if (action.type === CHANGE_LIFE_OF_LOAN) {
+    return { ...state, lifeOfLoan: action.lifeOfLoan };
+  }
+
+  if (action.type === CHANGE_PAYMENTS_PER_YEAR) {
+    return { ...state, paymentsPerYear: action.paymentsPerYear };
   }
 
   return state;
